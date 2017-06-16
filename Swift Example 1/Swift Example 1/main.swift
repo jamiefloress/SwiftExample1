89a6afe988f950fbf475a5f8aaeafecd4d5c6f24
jamiefloress <jamiefloress456@gmail.com>
2017-06-16 01:19:07 -0400

    Created several variable, printed values, &wrote for loop.

---
+++ Swift Example 1/Swift Example 1/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Swift Example 1
+//
+//  Created by Jamie Flores on 6/16/17.
+//  Copyright © 2017 Jamie Flores. All rights reserved.
+//
+
+import Foundation
+
+var name = "Jamie"
+var age = 20
+var value: Double = 123456
+
+print("Name is \(name)")
+print("\(name) is \(age) years old")
+
+for i in 1...5 {
+    print("i=\(i)")
+}
+
+
+
